a2342456ed4d30965de015dba21a795bf8543a23
Elshater <Elshater.Ehab@gmail.com>
2025-01-30 13:43:47 +0000

    all

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,32 +1,19 @@
-/**
- * Performs a mathematical operation on two rounded numbers
- * @param {string} type - The type of operation to perform (SUM, SUBTRACT,
- * or DIVIDE)
- * @param {number} a - The first number to be rounded and used in the
- * operation
- * @param {number} b - The second number to be rounded and used in the
- * operation
- * @returns {number|string} The result of the operation, or "Error" if
- * the operation is division and b is rounded to 0
- */
 function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
+  const n1 = Math.round(a);
+  const n2 = Math.round(b);
 
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB;
-    case 'SUBTRACT':
-      return roundedA - roundedB;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-
-    default:
-      throw new Error(`Invalid type: ${type}`);
+  if (type === 'SUBTRACT') {
+    return n1 - n2;
   }
+
+  if (type === 'DIVIDE') {
+    if (n2 === 0) {
+      return 'Error';
+    }
+    return n1 / n2;
+  }
+
+  return n1 + n2;
 }
 
 module.exports = calculateNumber;
