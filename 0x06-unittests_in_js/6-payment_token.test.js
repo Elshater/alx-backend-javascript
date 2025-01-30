d35064c9c8f32e8ba894d597cb6d7911091af410
Elshater <Elshater.Ehab@gmail.com>
2025-01-30 13:49:11 +0000

    all

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,30 +1,18 @@
-/* eslint-disable jest/expect-expect */
-/* eslint-disable jest/valid-expect */
+const mocha = require('mocha');
 const { expect } = require('chai');
-const assert = require('assert');
+const sinon = require('sinon');
 
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('returns a resolved promise with the object {data: "Successful response from the API"} when success is true', () => new Promise((done) => {
+  it('should resolve promise if success is true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
         done();
       })
-      .catch(done);
-  }));
-
-  // FAILING BELOW
-  // eslint-disable-next-line jest/no-commented-out-tests, jest/no-disabled-tests
-  it.skip('should return a rejected promise when success is false', () => new Promise((done) => {
-    getPaymentTokenFromAPI(false, (err) => {
-      try {
-        assert.strictEqual(err, 'Error');
-        done();
-      } catch (err) {
+      .catch((err) => {
         done(err);
-      }
-    });
-  }));
+      });
+  });
 });
